56b6d9be809ae1ffdef80b539d2a6dc8d10961f4
iwindd <freewinendlife@gmail.com>
2024-12-07 13:18:16 +0700

    feat: install mantine/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import type { Metadata } from "next";
+import { theme } from '../theme';
+import '@mantine/core/styles.css';
 
 export const metadata: Metadata = {
   title: "SVC Digital Safe",
@@ -11,9 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="th">
+    <html lang="th" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
+        <ColorSchemeScript />
+      </head>
       <body>
-        {children}
+        <MantineProvider theme={theme}>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
