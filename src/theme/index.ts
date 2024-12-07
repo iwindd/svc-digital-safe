f58e5c4d2ba4eb8e46c23c4b44fe4fa7dd81b987
iwindd <freewinendlife@gmail.com>
2024-12-07 14:43:05 +0700

    feat: add fonts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,16 @@
 'use client';
 
 import { createTheme } from '@mantine/core';
+import { Sarabun } from "next/font/google";
+
+const sarabun = Sarabun({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const theme = createTheme({
+  fontFamily: sarabun.style.fontFamily,
   breakpoints: {
     xs: '36em',
     sm: '48em',
