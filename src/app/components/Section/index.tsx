4c859fc922826e20e706ebe000f76b518df9016f
iwindd <freewinendlife@gmail.com>
2024-12-11 20:17:16 +0700

    refactor: section card header

--- src/app/components/Section/index.tsx
+++ src/app/components/Section/index.tsx
@@ -2,11 +2,12 @@
 
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Card, Title } from "@mantine/core";
+import { Card, Group, rem, ThemeIcon, Title } from "@mantine/core";
 import remarkGfm from "remark-gfm";
 import { Section } from "@/data/sections";
 import classes from "./style.module.css";
 import Liable from "./components/Liable";
+import { IconGavel } from "@tabler/icons-react";
 
 interface SectionProps {
   section: Section;
@@ -16,7 +17,12 @@ interface SectionProps {
 const Section = ({ section, number }: SectionProps) => {
   return (
     <Card shadow="md" padding="lg" radius="md">
-      <Title order={3}>มาตราที่ {number}</Title>
+      <Card.Section inheritPadding className={classes.headerSection}>
+        <Group>
+          <IconGavel />
+          <Title order={3}>มาตราที่ {number}</Title>
+        </Group>
+      </Card.Section>
       <div className={classes.paragraph}>
         <ReactMarkdown children={section.text} remarkPlugins={[remarkGfm]} />
       </div>
