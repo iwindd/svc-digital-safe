a2594006e457aec2474fed5996a9b7ad5618fc50
iwindd <freewinendlife@gmail.com>
2024-12-14 20:32:01 +0700

    feat: example case and offense

--- src/app/components/Section/index.tsx
+++ src/app/components/Section/index.tsx
@@ -1,13 +1,22 @@
-'use client';
+"use client";
 
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Card, Group, rem, ThemeIcon, Title } from "@mantine/core";
+import {
+  Accordion,
+  AspectRatio,
+  Card,
+  Group,
+  rem,
+  Text,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
 import remarkGfm from "remark-gfm";
 import { Section } from "@/data/sections";
 import classes from "./style.module.css";
 import Liable from "./components/Liable";
-import { IconGavel } from "@tabler/icons-react";
+import { IconBrandYoutube, IconGavel } from "@tabler/icons-react";
 
 interface SectionProps {
   section: Section;
@@ -26,8 +35,51 @@ const Section = ({ section, number }: SectionProps) => {
       <div className={classes.paragraph}>
         <ReactMarkdown children={section.text} remarkPlugins={[remarkGfm]} />
       </div>
-
-      {section.liable && <Liable liable={section.liable}/>}
+      {(section.cases || section.offense) && (
+        <Accordion 
+          variant="filled" 
+          radius="xs"
+          classNames={{
+            root: classes.accordion,
+            control: classes.accordionControl,
+            panel: classes.accordionPanel,
+            content: classes.accordionContent,
+          }}
+        >
+          {section.offense && (
+            <Accordion.Item value="offense">
+              <Accordion.Control>
+                <Text fs="italic" c="dimmed">ตัวอย่างการกระทำความผิดเกี่ยวกับพาณิชย์อิเล็กทรอนิกส์</Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <div>
+                  <ReactMarkdown children={section.offense} remarkPlugins={[remarkGfm]} />
+                </div>
+              </Accordion.Panel>
+            </Accordion.Item>
+          )}
+          {section.cases && section.cases.map((ytId, index) => (
+            <Accordion.Item key={ytId} value={ytId}>
+              <Accordion.Control>
+                <Text fs="italic" c="dimmed">กรณีศึกษา #{index + 1}</Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <AspectRatio ratio={16 / 9}>
+                  <iframe
+                    src={`https://www.youtube.com/embed/${ytId}`}
+                    className={classes.video}
+                    title="กรณีศึกษา"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                  />
+                </AspectRatio>
+              </Accordion.Panel>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
+      {section.liable && <Liable liable={section.liable} />}
     </Card>
   );
 };
