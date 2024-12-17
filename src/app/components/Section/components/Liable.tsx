f078f3c76816e0c86515acbc2c9feb3fc5278a05
iwindd <freewinendlife@gmail.com>
2024-12-17 19:16:41 +0700

    refactor: Liable component and extract utility functions for better readability

--- src/app/components/Section/components/Liable.tsx
+++ src/app/components/Section/components/Liable.tsx
@@ -1,71 +1,22 @@
-import type { Liable, LiableName, LiableValue } from '@/data/sections'
-import { Card, List, rem, SimpleGrid, ThemeIcon } from '@mantine/core'
-import { IconLock, IconReceipt } from '@tabler/icons-react'
+import type { Liable, LiableName } from "@/data/sections";
+import { Card, List, rem, SimpleGrid, ThemeIcon } from "@mantine/core";
+import { IconLock, IconReceipt } from "@tabler/icons-react";
 import classes from "../style.module.css";
-import React from 'react'
+import React from "react";
+import { Locales, parseLiable } from "@/lib/section";
 
 interface LiableProps {
-  liable: Liable
-}
-
-const Locales : Record<string, string> = {
-  "imprisonment": "จำคุก",
-  "fine": "ปรับ",
-  "fineUntil": "ปรับเป็นรายวันจนกว่าจะปฏิบัติถูกต้อง",
-  "<=": "ไม่เกิน",
-  "-": "ถึง"
-}
-
-const parseLiable = (liableValue : LiableValue, key: LiableName) => {
-  let [p1, p2, p3] : [any, any, any] = liableValue;
-  let result = "";
-
-  if (p3 != undefined) {
-    if (key == "imprisonment") {
-      p1 = formatDays(p1);
-      p3 = formatDays(p3);
-    }else{
-      p1 = formatMoney(p1);
-      p3 = formatMoney(p3);
-    }
-
-    result = `${p1} ${Locales[p2]} ${p3}`
-  }else{
-    if (key == "imprisonment") {
-      p2 = formatDays(p2);
-    }else{
-      p2 = formatMoney(p2);
-    }
-    result = `${Locales[p1]} ${p2}`;
-  }
-
-  if (key == "fineUntil") result += "/วัน";
-
-  return result;
-}
-
-function formatDays(days : number) : string {
-  const years = Math.floor(days / 365); // Calculate years
-  const remainingDaysAfterYears = days % 365; // Remaining days after years
-  const months = Math.floor(remainingDaysAfterYears / 30); // Calculate months
-
-  let result = "";
-  if (years > 0) result += `${years} ปี`;
-  if (months > 0) result += `${result ? " " : ""}${months} เดือน`;
-
-  return (result || "-"); // Default to "0 วัน" if no input days
-}
-
-function formatMoney(value: number): string {
-  return value.toLocaleString() + " บาท"
+  liable: Liable;
 }
 
 const Liable = (props: LiableProps) => {
-  const [allLiable] = React.useState(Array.isArray(props.liable) ? props.liable: [props.liable])
+  const [allLiable] = React.useState(
+    Array.isArray(props.liable) ? props.liable : [props.liable]
+  );
 
   return (
     <Card.Section inheritPadding className={classes.liableSection}>
-      <SimpleGrid 
+      <SimpleGrid
         cols={{
           base: 1,
           md: allLiable.length > 3 ? 3 : allLiable.length,
@@ -74,17 +25,17 @@ const Liable = (props: LiableProps) => {
         {allLiable.map((liableObj, liableIndex) => (
           <List key={liableIndex}>
             {Object.entries(liableObj).map(([key, value], index) => (
-              <List.Item 
+              <List.Item
                 key={`${liableIndex}-${index}`}
                 icon={
                   <ThemeIcon color="red" size={24} radius="xl">
-                    {
-                      key == "imprisonment" && (
-                        <IconLock style={{ width: rem(16), height: rem(16) }} />
-                      ) || (
-                        <IconReceipt style={{ width: rem(16), height: rem(16) }} />
-                      )
-                    }
+                    {(key == "imprisonment" && (
+                      <IconLock style={{ width: rem(16), height: rem(16) }} />
+                    )) || (
+                      <IconReceipt
+                        style={{ width: rem(16), height: rem(16) }}
+                      />
+                    )}
                   </ThemeIcon>
                 }
               >
@@ -95,7 +46,7 @@ const Liable = (props: LiableProps) => {
         ))}
       </SimpleGrid>
     </Card.Section>
-  )
-}
+  );
+};
 
-export default Liable
+export default Liable;
--- src/lib/section.ts
+++ src/lib/section.ts
@@ -1,4 +1,51 @@
-import sections, { Section } from "@/data/sections";
+import sections, { LiableName, LiableValue, Section } from "@/data/sections";
 
-export const getSections = () : Section[] => sections
-export const getSectionById = (id: number) : Section | undefined => sections.find(section => section.id === id)
+export const Locales: Record<string, string> = {
+  imprisonment: "จำคุก",
+  fine: "ปรับ",
+  fineUntil: "ปรับเป็นรายวันจนกว่าจะปฏิบัติถูกต้อง",
+  "<=": "ไม่เกิน",
+  "-": "ถึง",
+};
+
+export const getSections = (): Section[] => sections;
+export const getSectionById = (id: number): Section | undefined =>
+  sections.find((section) => section.id === id);
+
+export const formatMoney = (value: number): string => value.toLocaleString() + " บาท";
+export const formatDays = (days: number): string => {
+  const years = Math.floor(days / 365); // Calculate years
+  const remainingDaysAfterYears = days % 365; // Remaining days after years
+  const months = Math.floor(remainingDaysAfterYears / 30); // Calculate months
+
+  let result = "";
+  if (years > 0) result += `${years} ปี`;
+  if (months > 0) result += `${result ? " " : ""}${months} เดือน`;
+
+  return result || "-"; // Default to "0 วัน" if no input days
+};
+
+export const parseLiable = (
+  liableValue: LiableValue,
+  key: LiableName
+): string => {
+  const [p1, p2, p3] = liableValue;
+  let result = "";
+
+  const formatValue = (value: number, key: LiableName) => {
+    return key === "imprisonment" ? formatDays(value) : formatMoney(value);
+  };
+
+  if (p3 !== undefined) {
+    result = `${formatValue(p1 as number, key)} ${Locales[p2]} ${formatValue(
+      p3 as number,
+      key
+    )}`;
+  } else {
+    result = `${Locales[p1]} ${formatValue(p2 as unknown as number, key)}`;
+  }
+
+  if (key === "fineUntil") result += "/วัน";
+
+  return result;
+};
